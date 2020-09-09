4dbadf19e60a8331d277dcff89c730be95c69215
sbrook13 <sbrook13@gmail.com>
2020-09-09 15:32:58 -0600

    login page added

--- index.js
+++ index.js
@@ -6,36 +6,15 @@ fetch('http://localhost:3500/activities')
         const activitySection = document.querySelector('#activity-section')
         activities.forEach(activity=> {
             const activityCard = document.createElement('div')
-            const header = document.createElement('h2')
-            const image = document.createElement('a')
+            const header = document.createElement('h3')
+            const image = document.createElement('img')
 
             console.log(activity)
 
-            activityCard.classList.add('activity-card')
-            header.innerHTML = `<a class="activity-header" href="activity.html?id=${activity.id}">${activity.name}</a>`
-            // image.innerHTML = `<a href="activity.html?id=${activity.id}"><img src=${activity.image}></a>`
-            activityCard.style.backgroundImage = `url('${activity.image}')`
-            activityCard.append(header, image)
+            header.innerHTML = `<a href="activity.html?id=${activity.id}">${activity.name}</a>`
+            activityCard.style.backgroundImage = `url("${activity.image}")`
+
+            activityCard.append(header)
             activitySection.append(activityCard)
         })
     })
-
-//functions for pyramid movement
-    anime({
-        targets: 'div.middle',
-        translateY: [
-            {value: 110, duration: 1000},
-        
-        ],
-        delay: 500
-        })
-
-    anime({
-        targets: 'div.bottom',
-        translateY: [
-            {value: 220, duration: 1800},
-        ],
-        delay: 1100
-        })
-    
-    
