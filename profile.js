97bc11fa66c2c4f096489702e4777cc370d1a4b3
sbrook13 <sbrook13@gmail.com>
2020-09-10 14:57:25 -0600

    updated pack and profile pages first edit

--- profile.js
+++ profile.js
@@ -14,57 +14,29 @@ fetch(`http://localhost:3500/users/${user_id}`)
         profileHeader.append(h1)
     })
 
-fetch('http://localhost:3500/activities')
-    .then(response => response.json())
-    .then(activities => {
-        const activitySection = document.querySelector('#activity-section')
-        activities.forEach(activity=> {
-            const activityCard = document.createElement('div')
-            const header = document.createElement('h3')
-
-            console.log(activity)
-            activityCard.classList.add('activity-card')
-            header.innerHTML = `<a href="activity.html?user_id=${user_id}&activity_id=${activity.id}">${activity.name}</a>`
-            activityCard.style.backgroundImage = `url("${activity.image}")`
-
-            activityCard.append(header)
-            activitySection.append(activityCard)
-        })
-    })
-
 fetch('http://localhost:3500/packs')
     .then(response => response.json())
-    .then(packedBags => {
-        const bags = document.querySelector('#packed-bags')
+    .then(packs => {
+        const packSection = document.querySelector('#pack-section')
 
-        const listNames = new Set()
-        packedBags.forEach(packedBag =>{
-            listNames.add(packedBag.list_name) 
+        const packNames = new Set()
+        packs.forEach(pack => {
+            if (pack.user_id == user_id) {
+                packNames.add(pack.name) 
+            }
         })
-       
-        listNames.forEach(name =>{
-            const listCard = document.createElement('div')
+
+        packNames.forEach(pack => {
+            const packCard = document.createElement('div')
             const subtitle = document.createElement('h3')
-             
 
-            listCard.id = name.toLowerCase()
-            listCard.classList.add('list-card')
-            subtitle.textContent = name
-            listCard.append(subtitle)
-
-            bags.append(listCard)
-        })
-
-        packedBags.forEach(packedBag => {
-            const bagCard = document.querySelector(`#${packedBag.list_name.toLowerCase()}`)
-            const gearItem =  document.createElement('p')
-        
-            gearItem.classList.add('gear-bullet')
-            gearItem.innerHTML = `${packedBag.gear.name}`  
- 
+            packCard.classList.add('pack-card')
+            subtitle.innerHTML = `<a href="pack.html?user_id=${user_id}&pack_id=${pack.id}">${pack.name}</a>`
             
-            bagCard.append(gearItem)
- 
-         })
-        
+            packCard.append(subtitle)
+            packSection.append(packCard)
+        })
     })
+
+const updateForm = document.querySelector('#update-profile-button')
+updateForm.action = `http://localhost:3500/users/${user_id}`
